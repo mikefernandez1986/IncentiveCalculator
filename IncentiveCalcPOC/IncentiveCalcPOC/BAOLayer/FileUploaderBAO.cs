255d89aea9b7d5b1294aab796c393a3b51e32104
Mike <freelancingworkweb@gmail.com>
2019-01-23 09:10:24 +0530

    Initial COmmit to dev

---
+++ IncentiveCalcPOC/IncentiveCalcPOC/BAOLayer/FileUploaderBAO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using IncentiveCalcPOC.DAOLayer;
+
+namespace IncentiveCalcPOC.BAOLayer
+{
+    public class FileUploaderBAO
+    {
+        FileUploaderDAO DAO = new FileUploaderDAO();
+        public bool UploadFile(string FileName, string FilePath, string tableName)
+        {
+            bool status = false;
+            try
+            {
+                long FileId = DAO.UploadFileDetails(FileName);
+                if(FileId > 0)
+                {
+                    DataTable dt = DAO.ConvertExcelToDataTable(FilePath);
+                    dt.Columns.Add("FileId", typeof(System.Int32));
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        dr["FileId"] = FileId;
+                    }
+                    bool _clearData = DAO.TruncateTable(tableName);
+                    if (_clearData)
+                    {
+                        status = DAO.BulkInsert(dt, tableName);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return status;
+        }
+    }
+}
---
+++ IncentiveCalcPOC/IncentiveCalcPOC/Entities/KPIEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IncentiveCalcPOC.Entities
+{
+    public class KPIEntities
+    {
+        public int CASAID { get; set; }
+        public string EmployeeCode { get; set; }
+        public string EmployeeName { get; set; }        
+        public int PerformanceScore { get; set; }
+        public int KPIRating { get; set; }
+    }
+}
